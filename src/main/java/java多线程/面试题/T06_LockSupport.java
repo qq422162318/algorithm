279c654b0c4488eaf484b71bef7c0d9282cf1e15
qq422162318 <422162318@qq.com>
2020-11-06 22:18:05 +0800

    大师傅大师傅

--- src/main/java/java多线程/面试题/T06_LockSupport.java
+++ src/main/java/java多线程/面试题/T06_LockSupport.java
@@ -4,8 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
 
-public class T05_CountDownLatch {
+public class T06_LockSupport {
 
     // 添加volatile，使t2能够得到通知
     volatile List lists = new ArrayList();
@@ -19,26 +20,23 @@ public class T05_CountDownLatch {
     }
 
     public static void main(String[] args) {
-        T05_CountDownLatch c = new T05_CountDownLatch();
+        T06_LockSupport c = new T06_LockSupport();
 
         CountDownLatch latch = new CountDownLatch(1);
-        CountDownLatch latch2 = new CountDownLatch(1);
 
-        new Thread(() -> {
+        Thread t2 = new Thread(() -> {
             System.out.println("t2启动");
             if (c.size() != 5) {
-                try {
-                    latch.await();
 
-                    //也可以指定等待时间
-                    //latch.await(5000, TimeUnit.MILLISECONDS);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                LockSupport.park();
+
             }
             System.out.println("t2 结束");
-            latch2.countDown();
-        }, "t2").start();
+
+
+        }, "t2");
+
+        t2.start();
 
         try {
             TimeUnit.SECONDS.sleep(1);
@@ -53,20 +51,14 @@ public class T05_CountDownLatch {
                 System.out.println("add " + i);
 
                 if (c.size() == 5) {
-                    // 打开门闩，让t2得以执行
-                    latch.countDown();
-                    try {
-                        latch2.await();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+                    LockSupport.unpark(t2);
                 }
 
-//				try {
-//					TimeUnit.SECONDS.sleep(1);
-//				} catch (InterruptedException e) {
-//					e.printStackTrace();
-//				}
+				/*try {
+					TimeUnit.SECONDS.sleep(1);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}*/
             }
 
         }, "t1").start();
