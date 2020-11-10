0e63c6ad3eb4df8d5b33ca863fab37d74d884cde
qq422162318 <422162318@qq.com>
2020-11-10 20:31:28 +0800

    大师傅大师傅

---
+++ src/main/java/jieti/likou82.java
@@ -0,0 +1,25 @@
+package jieti;
+
+public class likou82 {
+    public class ListNode{
+        int val;
+        ListNode next;
+        public ListNode(int x){
+            this.val=x;
+        }
+    }
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head==null||head.next==null){
+            return head;
+        }
+        if (head.val==head.next.val){
+            while(head.next!=null&&head.val==head.next.val){
+                head=head.next;
+            }
+            return deleteDuplicates(head.next);
+        }else{
+            head.next = deleteDuplicates(head.next);
+            return head;
+        }
+    }
+}
