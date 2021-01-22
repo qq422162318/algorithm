d365802761c37a3c6b81ac7042e895d552ad6a63
qq422162318 <422162318@qq.com>
2021-01-22 14:13:42 +0800

    大师傅大师傅

---
+++ src/main/java/算法题/likou99.java
@@ -0,0 +1,32 @@
+package 算法题;
+
+public class likou99 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+    public static void main(String[] args) {
+        // int[] roots={1,3, (Integer) null, (Integer) null, 2 };
+
+        TreeNode root= new TreeNode(1);
+        root.left=new TreeNode(3);
+        root.left.right=new TreeNode(2);
+        recoverTree(root);
+        int temp=t.val;
+        t.val=s.val;
+        s.val=temp;
+    }
+    static TreeNode pre,t,s;
+    public static void recoverTree(TreeNode root) {
+        if(root==null)return ;
+        recoverTree(root.left);
+        if(pre!=null&&root.val<pre.val){
+            s= (s==null)?pre:s;
+            t=root;
+        }
+        pre=root;
+        recoverTree(root.right);
+    }
+}
