1aba7d83c188fa734e15dfbfc44e7e1c2533b4e8
qq422162318 <422162318@qq.com>
2021-04-01 16:06:04 +0800

    mylinux

--- src/main/java/算法题/likou130.java
+++ src/main/java/算法题/likou130.java
@@ -20,18 +20,18 @@ package 算法题;
 public class likou130 {
     public static void main(String[] args) {
         likou130 likou130 = new likou130();
-        char[][] board=new char[][]{
-                {'X','X','X','X'},
-                {'X','O','O','X'},
-                {'X','X','O','X'},
-                {'X','O','X','X'}
+        char[][] board = new char[][]{
+                {'X', 'X', 'X', 'X'},
+                {'X', 'O', 'O', 'X'},
+                {'X', 'X', 'O', 'X'},
+                {'X', 'O', 'X', 'X'}
         };
         likou130.solve(board);
         int rr = board.length;
         int cc = board[0].length;
         for (int i = 0; i < rr; i++) {
             for (int i1 = 0; i1 < cc; i1++) {
-                System.out.print(board[i][i1]+"\t");
+                System.out.print(board[i][i1] + "\t");
             }
             System.out.println();
         }
@@ -41,18 +41,18 @@ public class likou130 {
     int col;
 
     public void solve(char[][] board) {
-        if (board==null||board.length==0)return;
+        if (board == null || board.length == 0) return;
         row = board.length;
         col = board[0].length;
         //对所有行第一列，最后一列遍历O并替换为-
         for (int i = 0; i < row; i++) {
-            dfs(board,i,0);
-            dfs(board,i,col-1);
+            dfs(board, i, 0);
+            dfs(board, i, col-1);
         }
         //对第一行，最后一行遍历O并替换为-
-        for (int i = 0; i <col ; i++) {
-            dfs(board,0,i);
-            dfs(board,row-1,i);
+        for (int i = 0; i < col; i++) {
+            dfs(board, 0, i);
+            dfs(board, row - 1, i);
         }
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
@@ -61,19 +61,55 @@ public class likou130 {
             }
         }
     }
-    
+
     private void dfs(char[][] board, int i, int j) {
-        if (i < 0||i>=row || j< 0||j>=col || board[i][j]!='O')return;
-        board[i][j]='-';
-        dfs(board, i-1, j);
-        dfs(board, i+1, j);
-        dfs(board, i, j-1);
-        dfs(board, i, j+1);
+        if (i < 0 || i >= row || j < 0 || j >= col || board[i][j] != 'O') return;
+        board[i][j] = '-';
+        dfs(board, i - 1, j);
+        dfs(board, i + 1, j);
+        dfs(board, i, j - 1);
+        dfs(board, i, j + 1);
         return;
     }
+
     public void solve2(char[][] board) {
-        if (board.length==0)return ;
-        int m=board.length,n=board[0].length;
-        final UF uf = new UF();
+        if (board.length == 0) return;
+        int m = board.length, n = board[0].length;
+        UF uf = new UF(m * n + 1);
+        int dummy = m * n;
+        // 将首列和末列的 O 与 dummy 连通
+        for (int i = 0; i < m; i++) {
+            if (board[i][0] == 'O') uf.union(i * n, dummy);
+            if (board[i][n - 1] == 'O') uf.union(i * n + n - 1, dummy);
+        }
+        // 将首行和末行的 O 与 dummy 连通
+        for (int j = 0; j < n; j++) {
+            if (board[0][j] == 'O') uf.union(j, dummy);
+            if (board[m - 1][j] == 'O') uf.union(n * (m - 1) + j, dummy);
+        }
+        // 方向数组 d 是上下左右搜索的常用手法
+        int[][] d = new int[][]{{1, 0}, {0, 1}, {0, -1}, {-1, 0}};
+        for (int i = 0; i < m - 1; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                if (board[i][j] == 'O') {
+                    // 将此 O 与上下左右的 O 连通
+                    for (int k = 0; k < 4; k++) {
+                        int x = i + d[k][0];
+                        int y = j + d[k][1];
+                        if (board[x][y] == 'O') {
+                            uf.union(x * n + y, i * n + j);
+                        }
+                    }
+                }
+            }
+        }
+        // 所有不和 dummy 连通的 O，都要被替换
+        for (int i = 1; i < m - 1; i++) {
+            for (int j = 1; j < n - 1; j++) {
+                if (!uf.connected(dummy, i * n + j)) {
+                    board[i][j] = 'X';
+                }
+            }
+        }
     }
 }
