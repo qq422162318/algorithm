93b89b91bbb22657402b3334a82cb3d30c1d13ce
qq422162318 <422162318@qq.com>
2021-05-07 13:00:06 +0800

    fgg

--- src/main/java/java多线程/弱引用.java
+++ src/main/java/java多线程/弱引用.java
@@ -1,7 +1,9 @@
 package java多线程;
 
+import java.lang.ref.WeakReference;
+
 public class 弱引用 {
     public static void main(String[] args) {
-
+//        WeakReference weakReference = new WeakReference<>();
     }
 }
--- src/main/java/算法题/likou3.java
+++ src/main/java/算法题/likou3.java
@@ -1,26 +1,54 @@
 package 算法题;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.HashMap;
 
+/**
+ * 3. 无重复字符的最长子串
+ * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+ * 示例 1:
+ * 输入: s = "abcabcbb"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ * 示例 2:
+ * <p>
+ * 输入: s = "bbbbb"
+ * 输出: 1
+ * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ * 示例 3:
+ * <p>
+ * 输入: s = "pwwkew"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+ * 请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ * 示例 4:
+ * <p>
+ * 输入: s = ""
+ * 输出: 0
+ */
+@Slf4j
 public class likou3 {
     public static void main(String[] args) {
-       String s = "abcabcbb";
+        String s = "abcabcbb";
         likou3 likou3 = new likou3();
         System.out.println(likou3.lengthOfLongestSubstring(s));
     }
-    public int lengthOfLongestSubstring(String s){
+
+    public int lengthOfLongestSubstring(String s) {
         HashMap<Character, Integer> window = new HashMap<>();
-        int res=0,left=0,right=0;
-        while(right<s.length()){
+        int res = 0, left = 0, right = 0;
+        while (right < s.length()) {
             char c = s.charAt(right);
             right++;
-            window.put(c,window.getOrDefault(c,0)+1);
-            while (window.get(c)>1){
+            window.put(c, window.getOrDefault(c, 0) + 1);
+            while (window.get(c) > 1) {
                 char c1 = s.charAt(left);
                 left++;
-                window.put(c1,window.get(c1)-1);
+                window.put(c1, window.get(c1) - 1);
+                log.info("left=="+left);
             }
-            res=Math.max(res,right-left);
+            res = Math.max(res, right - left);
         }
         return res;
     }
