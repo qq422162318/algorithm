8d618c0f14c0f91a4f411e1503ab8ed3c6df688d
qq422162318 <422162318@qq.com>
2021-05-16 14:12:58 +0800

    modified:   likou316.java

--- src/main/java/算法题/likou316.java
+++ src/main/java/算法题/likou316.java
@@ -1,24 +1,46 @@
-package 算法题;
+package com.my;
 
-import java.util.Random;
+import org.springframework.expression.spel.SpelEvaluationException;
+
+import java.util.Stack;
 
 /**
  * 316. 去除重复字母
  * 给你一个字符串 s ，请你去除字符串中重复的字母，使得每个字母只出现一次。需保证 返回结果的字典序最小（要求不能打乱其他字符的相对位置）。
+ * <p>
  * 注意：该题与 1081 https://leetcode-cn.com/problems/smallest-subsequence-of-distinct-characters 相同
  * 示例 1：
  * 输入：s = "bcabc"
  * 输出："abc"
  * 示例 2：
+ * <p>
  * 输入：s = "cbacdcbc"
  * 输出："acdb"
  */
 public class likou316 {
     public String removeDuplicateLetters(String s) {
-       return null;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(new Random().nextInt(3));
+        Stack<Character> stack = new Stack<>();
+        int[] count = new int[256];
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i)]++;
+        }
+        boolean[] inStack = new boolean[256];
+        for (char c : s.toCharArray()) {
+            count[c]--;
+            if (inStack[c]) continue;
+            while (!stack.isEmpty()&&stack.peek()>c) {
+                if (count[stack.peek()] == 0) {
+                    break;
+                }
+                inStack[stack.pop()] = false;
+            }
+            stack.push(c);
+            inStack[c]=true;
+        }
+        StringBuilder sb = new StringBuilder();
+        while(!stack.isEmpty()){
+            sb.append(stack.pop());
+        }
+        return sb.reverse().toString();
     }
 }
