ad3c0d4c3e8d5e8f7d6085972d18e0837ebf8a2e
qq422162318 <422162318@qq.com>
2021-05-17 14:45:40 +0800

    jkl

---
+++ src/main/java/算法题/likou283.java
@@ -0,0 +1,30 @@
+package 算法题;
+
+/**
+ * 283. 移动零
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: [0,1,0,3,12]
+ * 输出: [1,3,12,0,0]
+ * 说明:
+ * <p>
+ * 必须在原数组上操作，不能拷贝额外的数组。
+ * 尽量减少操作次数。
+ */
+public class likou283 {
+    public void moveZeroes(int[] nums) {
+        if (nums.length==0)return;
+        int slow=0,fast=0;
+        while (fast<nums.length){
+            if (nums[fast]!=0){
+                nums[slow++]=nums[fast];
+            }
+            fast++;
+        }
+        for (; slow <nums.length ; slow++) {
+            nums[slow]=0;
+        }
+    }
+}
