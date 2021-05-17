28810bc37477218c27609eb909b494f57f573174
qq422162318 <422162318@qq.com>
2021-05-17 15:02:05 +0800

    jkl

--- src/main/java/算法题/likou83.java
+++ src/main/java/算法题/likou83.java
@@ -1,4 +1,5 @@
 package 算法题;
+
 /**
  * 83. 删除排序链表中的重复元素
  * 给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
@@ -10,25 +11,42 @@ package 算法题;
  * 输出: 1->2->3
  */
 public class likou83 {
-    public class ListNode{
+    public class ListNode {
         int val;
         ListNode next;
-        public ListNode(int x){
-            this.val=x;
+
+        public ListNode(int x) {
+            this.val = x;
         }
     }
+
     public ListNode deleteDuplicates(ListNode head) {
-        if (head==null||head.next==null){
+        if (head == null) return null;
+        ListNode slow = head, fast = head;
+        while (fast != null) {
+            if (fast.val != slow.val) {
+                slow.next = fast;
+                slow = slow.next;
+            }
+            fast = fast.next;
+        }
+        return head;
+    }
+
+    public ListNode deleteDuplicates2(ListNode head) {
+        if (head == null || head.next == null) {
             return head;
         }
-        if (head.val==head.next.val){
-            while(head.next!=null&&head.val==head.next.val){
-                head=head.next;
+        if (head.val == head.next.val) {
+            while (head.next != null && head.val == head.next.val) {
+                head = head.next;
             }
-            return deleteDuplicates(head);
-        }else{
-            head.next = deleteDuplicates(head.next);
+            return deleteDuplicates2(head);
+        } else {
+            head.next = deleteDuplicates2(head.next);
             return head;
         }
     }
+
+
 }
