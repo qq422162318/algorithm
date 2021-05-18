34153aea3dec01e4b6994f31440cf6c7bc66ce2f
qq422162318 <422162318@qq.com>
2021-05-18 15:52:24 +0800

    modified:   likou322.java

--- src/main/java/算法题/likou322.java
+++ src/main/java/算法题/likou322.java
@@ -1,5 +1,7 @@
 package 算法题;
 
+import java.util.Arrays;
+
 /**
  * 322. 零钱兑换
  * 给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回 -1。
@@ -28,25 +30,34 @@ package 算法题;
 public class likou322 {
     public static void main(String[] args) {
         likou322 likou322 = new likou322();
-        int[] coins = {1, 2, 5};
-        memo = new int[100];
-        for (int i = 0; i < memo.length; i++) {
-            memo[i] = 0;
-        }
-        int i = likou322.coinChange2(coins, 11);
+        int[] coins = {186, 419, 83, 408};
+        int i = likou322.coinChange2(coins, 6249);
         System.out.println(i);
     }
 
-    static int[] memo;
+    int[] memo;
 
-    //自顶向下
+    /**
+     * 自顶向下 (数值一大直接超时)
+     *
+     * @param coins
+     * @param amount
+     * @return
+     */
     public int coinChange(int[] coins, int amount) {
+        if (amount == 0) return 0;
+        if (amount < 0) return -1;
+        memo = new int[amount + 1];
+        return help(coins, amount);
+    }
+
+    public int help(int[] coins, int amount) {
         if (amount == 0) return 0;
         if (amount < 0) return -1;
         if (memo[amount] != 0) return memo[amount];
         int res = Integer.MAX_VALUE;
         for (int i = 0; i < coins.length; i++) {
-            int subproblem = coinChange(coins, amount - coins[i]);
+            int subproblem = help(coins, amount - coins[i]);
             if (subproblem == -1) continue;
             res = Math.min(res, 1 + subproblem);
         }
@@ -57,22 +68,21 @@ public class likou322 {
 
     /**
      * dp 自底向上
-     * @param coins 硬币面额数
+     *
+     * @param coins  硬币面额数
      * @param amount 要凑的数
      * @return
      */
     public int coinChange2(int[] coins, int amount) {
-        memo = new int[amount + 1];
-        for (int i = 0; i < memo.length; i++) {
-            memo[i] = amount + 1;
-        }
-        memo[0] = 0;
-        for (int i = 0; i < memo.length; i++) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp,amount+1);
+        dp[0] = 0;
+        for (int i = 0; i < dp.length; i++) {
             for (int coin : coins) {
                 if (i - coin < 0) continue;
-                memo[i] = Math.min(memo[i], 1 + memo[i - coin]);
+                dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
             }
         }
-        return (memo[amount] == amount + 1) ? -1 : memo[amount];
+        return (dp[amount] == amount + 1) ? -1 : dp[amount];
     }
 }
