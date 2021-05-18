b8d59ff606cbb7f97c5cf50236b17f295018d579
qq422162318 <422162318@qq.com>
2021-05-18 12:58:35 +0800

    modified:   problem10_1.java

--- src/main/java/算法题/剑指offer/problem10_1.java
+++ src/main/java/算法题/剑指offer/problem10_1.java
@@ -35,4 +35,13 @@ public class problem10_1 {
         }
         return a;
     }
+
+    public int fib2(int n) {
+        if (n == 0) return n;
+        int[] dp = new int[n + 1];
+        for (int i = 3; i < n; i++) {
+            dp[i]=(dp[i-1]+dp[i-2])%1000000007;
+        }
+        return dp[n];
+    }
 }
