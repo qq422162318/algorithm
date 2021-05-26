c0c3dee99b0b4781daa58a114109a04e4044bdb6
qq422162318 <422162318@qq.com>
2021-05-26 09:37:02 +0800

    jkl

--- src/main/java/算法题/likou494.java
+++ src/main/java/算法题/likou494.java
@@ -2,6 +2,7 @@ package 算法题;
 
 import javax.management.modelmbean.InvalidTargetObjectTypeException;
 import java.util.HashMap;
+import java.util.concurrent.ForkJoinPool;
 
 /**
  * 494. 目标和
@@ -29,10 +30,13 @@ import java.util.HashMap;
  */
 public class likou494 {
     public static void main(String[] args) {
-        int[] nums={1,1,1,1,1};
-        System.out.println(new likou494().findTargetSumWays(nums,3));
+        int[] nums={1};
+        System.out.println(new likou494().findTargetSumWays3(nums,1));
     }
 
+    /**
+     * 回溯法
+     */
     int result = 0;
     public int findTargetSumWays(int[] nums, int target) {
         if (nums.length == 0) return 0;
@@ -53,14 +57,18 @@ public class likou494 {
         target -= nums[i];
         backTrack(nums, i + 1, target);
         target += nums[i];
-
     }
 
+    /**
+     * 备忘录
+     * @param nums
+     * @param target
+     * @return
+     */
     int findTargetSumWays2(int[] nums, int target) {
         if (nums.length == 0) return 0;
         return dp(nums, 0, target);
     }
-    // 备忘录
     HashMap<String, Integer> memo = new HashMap<>();
     int dp(int[] nums, int i, int rest) {
         // base case
@@ -79,5 +87,51 @@ public class likou494 {
         // 记入备忘录
         memo.put(key, result);
         return result;
+    }
+
+    /**
+     * 动态规划(转化为背包问题)
+     * @param nums
+     * @param target
+     * @return
+     */
+    int findTargetSumWays3(int[] nums, int target) {
+        int sum=0;
+        for (int num : nums) {
+            sum+=num;
+        }
+        if (sum<target||(sum+target)%2==1)return 0;
+        return subsets2(nums,(sum+target)/2);
+    }
+    private int subsets(int[] nums, int sum) {
+        int n=nums.length;
+        int[][] dp=new int[n+1][sum+1];
+        for (int i = 0; i < n; i++) {
+            dp[i][0]=1;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= sum; j++) {
+                if (j>=nums[i-1])
+                    dp[i][j]=dp[i-1][j]+dp[i-1][j-nums[i-1]];
+                else
+                    dp[i][j]=dp[i-1][j];
+            }
+        }
+        return dp[n][sum];
+    }
+    //beat优化dp
+    private int subsets2(int[] nums, int sum) {
+        int n=nums.length;
+        int[] dp=new int[sum+1];
+        dp[0]=1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = sum; j >= 0; j--) {
+                if (j>=nums[i-1])
+                    dp[j]=dp[j]+dp[j-nums[i-1]];
+                else
+                    dp[j]=dp[j];
+            }
+        }
+        return dp[sum];
     }
 }
