2be4945f85233b18bab23f07b1dbd63a4470d325
qq422162318 <422162318@qq.com>
2021-05-29 13:47:11 +0800

    jkl

--- src/main/java/算法题/NodeList.java
+++ src/main/java/算法题/NodeList.java
@@ -1,18 +1,21 @@
 package 算法题;
 
 public class NodeList {
-  private Node head;
-  public class Node{
-       Node item;
-       Node next;
-       private Node(Node item,Node next){
-           this.item=item;
-           this.next=next;
-       }
-  }
-  public NodeList(Node node,Node item){
-      head.next=node;
-      head.item=item;
-  }
+    private Node head;
+
+    public class Node {
+        Node item;
+        Node next;
+
+        private Node(Node item, Node next) {
+            this.item = item;
+            this.next = next;
+        }
+    }
+
+    public NodeList(Node node, Node item) {
+        head.next = node;
+        head.item = item;
+    }
 
 }
