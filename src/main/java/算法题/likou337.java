9a194d28a35cd343e1a55e1203e4d5c3e96e0603
qq422162318 <422162318@qq.com>
2021-06-16 17:10:19 +0800

    jkl

--- src/main/java/算法题/likou337.java
+++ src/main/java/算法题/likou337.java
@@ -3,7 +3,9 @@ import java.util.HashMap;
 import java.util.Map;
 /**
  * 337. 打家劫舍 III
- * 在上次打劫完一条街道之后和一圈房屋后，小偷又发现了一个新的可行窃的地区。这个地区只有一个入口，我们称之为“根”。 除了“根”之外，每栋房子有且只有一个“父“房子与之相连。一番侦察之后，聪明的小偷意识到“这个地方的所有房屋的排列类似于一棵二叉树”。 如果两个直接相连的房子在同一天晚上被打劫，房屋将自动报警。
+ * 在上次打劫完一条街道之后和一圈房屋后，小偷又发现了一个新的可行窃的地区。这个地区只有一个入口，我们称之为“根”。
+ * 除了“根”之外，每栋房子有且只有一个“父“房子与之相连。一番侦察之后，聪明的小偷意识到“这个地方的所有房屋的排列类似于一棵二叉树”。
+ * 如果两个直接相连的房子在同一天晚上被打劫，房屋将自动报警。
  * 计算在不触动警报的情况下，小偷一晚能够盗取的最高金额。
  * 示例 1:
  * 输入: [3,2,3,null,3,null,1]
@@ -38,6 +40,7 @@ public class likou337 {
         treeNode.left=t4;treeNode.right=t5;treeNode.right.right=treeNode1;treeNode.left.left=treeNode2;treeNode.left.right=t3;
         System.out.println(likou337.rob2(treeNode));
     }
+
     Map<TreeNode,Integer> memo=new HashMap<>();
     public int rob(TreeNode root) {
         if (root==null)return 0;
@@ -51,6 +54,7 @@ public class likou337 {
         memo.put(root,res);
         return res;
     }
+
     public int rob2(TreeNode root){
         int[] res=dp(root);
         return Math.max(res[0],res[1]);
