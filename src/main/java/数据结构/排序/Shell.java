dc5b8f23f572e2823a82105ebe8f554c4e527fe0
qq422162318 <422162318@qq.com>
2021-06-18 16:40:56 +0800

    jkl

--- src/main/java/数据结构/排序/Shell.java
+++ src/main/java/数据结构/排序/Shell.java
@@ -1,26 +1,27 @@
 package 数据结构.排序;
+
 //希尔排序
 public class Shell {
     public static void sort(Comparable[] a) {
-       int h=1;
-       while(h<a.length/2){
-           h=2*h+1;
-       }
-       while (h>=1){
-           //找到待插入的元素
-       for (int i=h;i<a.length-1;i++){
-           //把待插入的元素插入到有序数列中
-           for (int j = i; j>=h ; j-=h) {
-               if (greater(a[j-h],a[j])){
-                   exch(a,j-h,j);
-               }else{
-                   break;
-               }
-           }
-       }
+        int h = 1;
+        while (h < a.length / 2) {
+            h = 2 * h + 1;
+        }
+        while (h >= 1) {
+            //找到待插入的元素
+            for (int i = h; i < a.length - 1; i++) {
+                //把待插入的元素插入到有序数列中
+                for (int j = i; j >= h; j -= h) {
+                    if (greater(a[j - h], a[j])) {
+                        exch(a, j - h, j);
+                    } else {
+                        break;
+                    }
+                }
+            }
 
-           h=h/2;
-       }
+            h = h / 2;
+        }
     }
 
     private static boolean greater(Comparable v, Comparable w) {
