5d70587fd4bcfa6647d918ff8d349ca1961db9a5
qq422162318 <422162318@qq.com>
2021-06-18 16:40:43 +0800

    jkl

--- src/main/java/数据结构/排序/Selection.java
+++ src/main/java/数据结构/排序/Selection.java
@@ -1,28 +1,29 @@
 package 数据结构.排序;
+
 //选择排序
 public class Selection {
     //选择排序
-    public static void sort(Comparable[] a){
-        for (int i = 0; i <=a.length-2 ; i++) {
-            int minIndex=i;
-            for (int j = i+1; j < a.length; j++) {
-                if (greater(a[minIndex],a[j])){
-                    minIndex=j;
+    public static void sort(Comparable[] a) {
+        for (int i = 0; i <= a.length - 2; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < a.length; j++) {
+                if (greater(a[minIndex], a[j])) {
+                    minIndex = j;
                 }
             }
-            exch(a,i,minIndex);
+            exch(a, i, minIndex);
         }
     }
 
-    private static boolean greater(Comparable v,Comparable w) {
-        return v.compareTo(w)>0;
+    private static boolean greater(Comparable v, Comparable w) {
+        return v.compareTo(w) > 0;
     }
 
-    private static void exch(Comparable[] a, int i, int j){
+    private static void exch(Comparable[] a, int i, int j) {
         Comparable temp;
-        temp=a[i];
-        a[i]=a[j];
-        a[j]=temp;
+        temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
 
     }
 }
