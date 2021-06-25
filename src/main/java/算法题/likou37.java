685a5cffc521f1d065bb92d23767dd705917dff7
qq422162318 <422162318@qq.com>
2021-06-25 13:00:23 +0800

    jkl

--- src/main/java/算法题/likou37.java
+++ src/main/java/算法题/likou37.java
@@ -3,9 +3,9 @@ package 算法题;
 /**
  * 37. 解数独
  * 编写一个程序，通过已填充的空格来解决数独问题。
- *
+ * <p>
  * 一个数独的解法需遵循如下规则：
- *
+ * <p>
  * 数字 1-9 在每一行只能出现一次。
  * 数字 1-9 在每一列只能出现一次。
  * 数字 1-9 在每一个以粗实线分隔的 3x3 宫内只能出现一次。
@@ -24,36 +24,39 @@ public class likou37 {
                 {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
                 {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
         };
-        solveSudoku(board);
-
+        likou37 likou37 = new likou37();
+        likou37.solveSudoku(board);
 //        for (char[] chars : board) {
 //            System.out.println(chars);;
 //        }
-        for (int i = 0; i <board.length ; i++) {
-            for (int j = 0; j <board[0].length ; j++) {
-                System.out.print(board[i][j]+"\t");
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                System.out.print(board[i][j] + "\t");
             }
             System.out.println();
         }
     }
-    public static void solveSudoku(char[][] board) {
-        if (board==null||board.length==0||board[0].length==0){
+
+    public void solveSudoku(char[][] board) {
+        if (board == null || board.length == 0 || board[0].length == 0) {
             return;
         }
-        boolean solve = solve(board);
-        System.out.println(solve);
+//        boolean solve = solve(board);
+        boolean solve2 = backtrack(board,0,0);
+        System.out.println(solve2);
     }
-    public static boolean solve(char[][] board){
-        for (int i = 0; i <board.length ; i++) {
-            for (int j = 0; j <board[0].length ; j++) {
-                if (board[i][j]=='.'){
-                    for(char num='1';num<='9';num++){
-                        if (isValid(board,i,j,num)){
-                          board[i][j]=num;
-                            if(solve(board)) {
+
+    public boolean solve(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == '.') {
+                    for (char num = '1'; num <= '9'; num++) {
+                        if (isValid(board, i, j, num)) {
+                            board[i][j] = num;
+                            if (solve(board)) {
                                 return true;
-                            }else{
-                                board[i][j]='.';
+                            } else {
+                                board[i][j] = '.';
                             }
                         }
                     }
@@ -63,20 +66,35 @@ public class likou37 {
         }
         return true;
     }
-    public static boolean isValid(char[][] board,int row,int col,char num){
-        for (int k = 0; k <9 ; k++) {
+
+    public boolean isValid(char[][] board, int row, int col, char num) {
+        for (int k = 0; k < 9; k++) {
             //判断列存在
-            if (board[k][col]==num){
-                return false;
-            }
+            if (board[k][col] == num) return false;
             //判断行存在
-            if (board[row][k]==num){
+            if (board[row][k] == num) return false;
+            char chars = board[3 * (row / 3) + k / 3][3 * (col / 3) + k % 3];
+            if (chars == num)
                 return false;
-            }
-            if (board[3*(row/3)+k/3][3*(col/3)+k%3]==num){
-                return false;
-            }
         }
         return true;
+    }
+
+    boolean backtrack(char[][] board, int i, int j) {
+        int m = 9, n = 9;
+        if (i == m) return true;
+        if (j == n) {
+            return backtrack(board, i + 1, 0);
+        }
+        if (board[i][j] != '.') {
+            return backtrack(board, i, j + 1);
+        }
+        for (char ch = '1'; ch <= '9'; ch++) {
+            if (!isValid(board, i, j, ch)) continue;
+            board[i][j] = ch;
+            if (backtrack(board, i, j + 1)) return true;
+            board[i][j] = '.';
+        }
+        return false;
     }
 }
--- src/main/java/算法题/likou77.java
+++ src/main/java/算法题/likou77.java
@@ -1,8 +1,10 @@
 package 算法题;
+
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
 import java.util.List;
+
 /**
  * 77. 组合
  * 给定两个整数 n 和 k，返回 1 ... n 中所有可能的 k 个数的组合。
@@ -10,12 +12,12 @@ import java.util.List;
  * 输入: n = 4, k = 2
  * 输出:
  * [
- *   [2,4],
- *   [3,4],
- *   [2,3],
- *   [1,2],
- *   [1,3],
- *   [1,4],
+ * [2,4],
+ * [3,4],
+ * [2,3],
+ * [1,2],
+ * [1,3],
+ * [1,4],
  * ]
  */
 public class likou77 {
@@ -26,6 +28,7 @@ public class likou77 {
             System.out.println(integers);
         }
     }
+
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> res = new ArrayList<>();
         if (k <= 0 || n < k) {
@@ -36,19 +39,16 @@ public class likou77 {
         dfs(n, k, 1, path, res);
         return res;
     }
+
     private void dfs(int n, int k, int begin, Deque<Integer> path, List<List<Integer>> res) {
-        // 递归终止条件是：path 的长度等于 k
         if (path.size() == k) {
             res.add(new ArrayList<>(path));
             return;
         }
         // 遍历可能的搜索起点
         for (int i = begin; i <= n; i++) {
-            // 向路径变量里添加一个数
             path.addLast(i);
-            // 下一轮搜索，设置的搜索起点要加 1，因为组合数理不允许出现重复的元素
             dfs(n, k, i + 1, path, res);
-            // 重点理解这里：深度优先遍历有回头的过程，因此递归之前做了什么，递归之后需要做相同操作的逆向操作
             path.removeLast();
         }
     }
