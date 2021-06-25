7722f81c9f80c927edb9dd37da69f2637f499ed8
qq422162318 <422162318@qq.com>
2021-06-25 14:42:02 +0800

    jkl

--- src/main/java/算法题/likou111.java
+++ src/main/java/算法题/likou111.java
@@ -3,38 +3,69 @@ package 算法题;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**
+ * 111. 二叉树的最小深度
+ * 给定一个二叉树，找出其最小深度。
+ * 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+ * 说明：叶子节点是指没有子节点的节点。
+ * 示例 1：
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：2
+ * 示例 2：
+ * 输入：root = [2,null,3,null,4,null,5,null,6]
+ * 输出：5
+ * 提示：
+ * 树中节点数的范围在 [0, 105] 内
+ * -1000 <= Node.val <= 1000
+ */
 public class likou111 {
-      public class TreeNode {
-      int val;
-      TreeNode left;
-      TreeNode right;
-      TreeNode() {}
-      TreeNode(int val) { this.val = val; }
-      TreeNode(int val, TreeNode left, TreeNode right) {
-          this.val = val;
-          this.left = left;
-          this.right = right;
-      }
-  }
-
     public static void main(String[] args) {
         likou111 likou = new likou111();
     }
+
     public int minDepth(TreeNode root) {
-       if (root==null)return 0;
-        Queue<TreeNode> queue=new LinkedList<>();
+        if (root == null) return 0;
+        if (root.left != null && root.right == null)
+            return 1 + minDepth(root.left);
+        if (root.right != null && root.left == null)
+            return 1 + minDepth(root.right);
+        return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+    }
+
+    public int minDepth2(TreeNode root) {
+        if (root == null) return 0;
+        Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
-        int depth=1;
-        while(!queue.isEmpty()){
-            int size=queue.size();
-            for (int i = 0; i <size ; i++) {
+        int depth = 1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
                 TreeNode head = queue.poll();
-                if (head.left==null&&head.right==null)return depth;
-                if (head.left!=null)queue.offer(head.left);
-                if (head.right!=null)queue.offer(head.right);
+                if (head.left == null && head.right == null) return depth;
+                if (head.left != null) queue.offer(head.left);
+                if (head.right != null) queue.offer(head.right);
             }
             depth++;
         }
         return depth;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
     }
 }
