30acc9742df8ec434108ad1875f1f43b83ba2453
qq422162318 <422162318@qq.com>
2021-07-01 15:11:33 +0800

    jkl

---
+++ src/main/java/算法题/likou448.java
@@ -0,0 +1,19 @@
+package 算法题;
+
+import java.util.List;
+
+/**
+ * 448. 找到所有数组中消失的数字
+ * 给你一个含 n 个整数的数组 nums ，其中 nums[i] 在区间 [1, n] 内。请你找出所有在 [1, n] 范围内但没有出现在 nums 中的数字，并以数组的形式返回结果。
+ * 示例 1：
+ * 输入：nums = [4,3,2,7,8,2,3,1]
+ * 输出：[5,6]
+ * 示例 2：
+ * 输入：nums = [1,1]
+ * 输出：[2]
+ */
+public class likou448 {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+
+    }
+}
