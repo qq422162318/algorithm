aecf4a992731a12f8750b7c37cfba8820a65e4d9
qq422162318 <422162318@qq.com>
2021-07-06 19:47:21 +0800

    jkl

--- src/main/java/算法题/likou382.java
+++ src/main/java/算法题/likou382.java
@@ -5,18 +5,14 @@ import java.util.Random;
 /**
  * 382. 链表随机节点
  * 给定一个单链表，随机选择链表的一个节点，并返回相应的节点值。保证每个节点被选的概率一样。
- * <p>
  * 进阶:
  * 如果链表十分大且长度未知，如何解决这个问题？你能否使用常数级空间复杂度实现？
- * <p>
  * 示例:
- * <p>
  * // 初始化一个单链表 [1,2,3].
  * ListNode head = new ListNode(1);
  * head.next = new ListNode(2);
  * head.next.next = new ListNode(3);
  * Solution solution = new Solution(head);
- * <p>
  * // getRandom()方法应随机返回1,2,3中的一个，保证每个元素被返回的概率相等。
  * solution.getRandom();
  */
@@ -65,7 +61,7 @@ public class likou382 {
             p = p.next;
         }
         int i = k;
-        
+
         while (p != null) {
             int j = r.nextInt(++i);
             if (j < k)
