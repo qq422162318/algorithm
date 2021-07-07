644e6cc120362f1648d5efbf1964e867e42b38dd
qq422162318 <422162318@qq.com>
2021-07-07 16:27:18 +0800

    jkl

--- src/main/java/算法题/likou283.java
+++ src/main/java/算法题/likou283.java
@@ -12,16 +12,16 @@ package 算法题;
  */
 public class likou283 {
     public void moveZeroes(int[] nums) {
-        if (nums.length==0)return;
-        int slow=0,fast=0;
-        while (fast<nums.length){
-            if (nums[fast]!=0){
-                nums[slow++]=nums[fast];
+        if (nums.length == 0) return;
+        int slow = 0, fast = 0;
+        while (fast < nums.length) {
+            if (nums[fast] != 0) {
+                nums[slow++] = nums[fast];
             }
             fast++;
         }
-        for (; slow <nums.length ; slow++) {
-            nums[slow]=0;
+        for (; slow < nums.length; slow++) {
+            nums[slow] = 0;
         }
     }
 }
