6e146f084edaa61af0a39ec393ae12bc58cca59b
qq422162318 <422162318@qq.com>
2021-07-11 10:51:14 +0800

    jkl

--- src/main/java/算法题/likou560.java
+++ src/main/java/算法题/likou560.java
@@ -1,5 +1,9 @@
 package 算法题;
 
+import org.omg.PortableInterceptor.INACTIVE;
+
+import java.util.HashMap;
+
 /**
  * 560. 和为K的子数组
  * 给定一个整数数组和一个整数 k，你需要找到该数组中和为 k 的连续的子数组的个数。
@@ -11,7 +15,42 @@ package 算法题;
  * 数组中元素的范围是 [-1000, 1000] ，且整数 k 的范围是 [-1e7, 1e7]。
  */
 public class likou560 {
-    public int subarraySum(int[] nums, int k) {
+    public static void main(String[] args) {
+        likou560 likou560 = new likou560();
+        int a[] = {1, 1, 1};
+        System.out.println(likou560.subarraySum2(a, 2));
+    }
 
+    public int subarraySum(int[] nums, int k) {
+        int n = nums.length;
+        int[] sum = new int[n + 1];
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            sum[i + 1] = sum[i] + nums[i];
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (sum[i] - sum[j] == k)
+                    ans++;
+            }
+        }
+        return ans;
+    }
+
+    public int subarraySum2(int[] nums, int k) {
+        int n = nums.length;
+        int ans = 0;
+        int sum_i = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        for (int i = 0; i < n; i++) {
+            sum_i = sum_i + nums[i];
+            int j = sum_i - k;
+            if (map.containsKey(j)) {
+                ans += map.get(j);
+            }
+            map.put(sum_i, map.getOrDefault(sum_i, 0) + 1);
+        }
+        return ans;
     }
 }
