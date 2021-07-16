44501d8b17511c514eca334f11953f0aa7053557
qq422162318 <422162318@qq.com>
2021-07-16 19:12:06 +0800

    jkl

---
+++ src/main/java/算法题/剑指offer/offer11.java
@@ -0,0 +1,28 @@
+package 算法题.剑指offer;
+
+/**
+ * 剑指 Offer 11. 旋转数组的最小数字
+ * 把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。输入一个递增排序的数组的一个旋转，输出旋转数组的最小元素。例如，数组 [3,4,5,1,2] 为 [1,2,3,4,5] 的一个旋转，该数组的最小值为1。
+ * 示例 1：
+ * 输入：[3,4,5,1,2]
+ * 输出：1
+ * 示例 2：
+ * 输入：[2,2,2,0,1]
+ * 输出：0
+ */
+public class offer11 {
+    public int minArray(int[] numbers) {
+        int left = 0, right = numbers.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (numbers[mid] > numbers[right]) {
+                left = mid + 1;
+            } else if (numbers[mid] < numbers[right]) {
+                right = mid;
+            } else {
+                right--;
+            }
+        }
+        return numbers[left];
+    }
+}
