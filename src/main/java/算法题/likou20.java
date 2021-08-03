7eb55ddd7d8dfea0cb80ddc1960cf6cb23bc9359
qq422162318 <422162318@qq.com>
2021-08-03 09:18:30 +0800

    jkl

--- src/main/java/算法题/likou20.java
+++ src/main/java/算法题/likou20.java
@@ -1,5 +1,8 @@
 package 算法题;
 
+import java.util.LinkedList;
+import java.util.Stack;
+
 /**
  * 20. 有效的括号
  * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
@@ -24,6 +27,23 @@ package 算法题;
  */
 public class likou20 {
     public boolean isValid(String s) {
+        int length = s.length() / 2;
+        for (int i = 0; i < length; i++)
+            s = s.replace("()", "")
+                    .replace("[]", "")
+                    .replace("{}", "");
+        if (s.length() == 0) return true;
+        return false;
+    }
 
+    public boolean isValid2(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') stack.push(')');
+            else if (c == '{') stack.push('}');
+            else if (c == '[') stack.push(']');
+            else if (stack.isEmpty() || c != stack.pop()) return false;
+        }
+        return stack.isEmpty();
     }
 }
