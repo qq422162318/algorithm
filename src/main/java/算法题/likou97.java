decad7b09cad091a2194f983b69731f43f6f8e2d
qq422162318 <422162318@qq.com>
2021-11-01 08:58:09 +0800

    jkl

---
+++ src/main/java/算法题/likou97.java
@@ -0,0 +1,50 @@
+package 算法题;
+
+/**
+ * 97. 交错字符串
+ * 给定三个字符串 s1、s2、s3，请你帮忙验证 s3 是否是由 s1 和 s2 交错 组成的。
+ * 两个字符串 s 和 t 交错 的定义与过程如下，其中每个字符串都会被分割成若干 非空 子字符串：
+ * s = s1 + s2 + ... + sn
+ * t = t1 + t2 + ... + tm
+ * |n - m| <= 1
+ * 交错 是 s1 + t1 + s2 + t2 + s3 + t3 + ... 或者 t1 + s1 + t2 + s2 + t3 + s3 + ...
+ * 提示：a + b 意味着字符串 a 和 b 连接。
+ * 示例 1：
+ * 输入：s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
+ * 输出：true
+ * 示例 2：
+ * 输入：s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
+ * 输出：false
+ * 示例 3：
+ * 输入：s1 = "", s2 = "", s3 = ""
+ * 输出：true
+ */
+public class likou97 {
+    public static void main(String[] args) {
+//        System.out.println(true & false);
+        likou97 likou97 = new likou97();
+        String s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac";
+        System.out.println(likou97.isInterleave(s1, s2, s3));
+    }
+
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if (s1 == "" && s2 == "" && s3 == "") return true;
+        if (s1.length() + s2.length() != s3.length()) return false;
+        boolean[][] dp = new boolean[s1.length() + 1][s2.length() + 1];
+        dp[0][0] = true;
+        for (int i = 1; i < s1.length(); i++) {
+            dp[i][0] = dp[i - 1][0] && (s1.charAt(i - 1) == s3.charAt(i - 1));
+        }
+        for (int i = 1; i < s1.length(); i++) {
+            dp[0][i] = dp[0][i - 1] && (s2.charAt(i - 1) == s3.charAt(i - 1));
+        }
+        for (int i = 1; i < s1.length(); i++) {
+            for (int j = 1; j < s2.length(); j++) {
+                dp[i][j] = (dp[i - 1][j] & s1.charAt(i) == s3.charAt(i + j - 1)) |
+                        (dp[i][j - 1] & s2.charAt(j - 1) == s3.charAt(i + j - 1));
+            }
+        }
+        return dp[s1.length()][s2.length()];
+    }
+
+}
