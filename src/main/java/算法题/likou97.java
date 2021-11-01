1accecde262890c401d9c55a7e120e7d4118745b
qq422162318 <422162318@qq.com>
2021-11-01 09:01:38 +0800

    jkl

--- src/main/java/算法题/likou97.java
+++ src/main/java/算法题/likou97.java
@@ -38,9 +38,9 @@ public class likou97 {
         for (int i = 1; i < s1.length(); i++) {
             dp[0][i] = dp[0][i - 1] && (s2.charAt(i - 1) == s3.charAt(i - 1));
         }
-        for (int i = 1; i < s1.length(); i++) {
-            for (int j = 1; j < s2.length(); j++) {
-                dp[i][j] = (dp[i - 1][j] & s1.charAt(i) == s3.charAt(i + j - 1)) |
+        for (int i = 1; i <= s1.length(); i++) {
+            for (int j = 1; j <= s2.length(); j++) {
+                dp[i][j] = (dp[i - 1][j] & s1.charAt(i - 1) == s3.charAt(i + j - 1)) |
                         (dp[i][j - 1] & s2.charAt(j - 1) == s3.charAt(i + j - 1));
             }
         }
