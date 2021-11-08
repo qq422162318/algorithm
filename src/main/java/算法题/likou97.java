fd97acbbdf7f12c65fa823de28fc6ce30012dacc
qq422162318 <422162318@qq.com>
2021-11-08 09:08:32 +0800

    jkl

--- src/main/java/算法题/likou97.java
+++ src/main/java/算法题/likou97.java
@@ -27,19 +27,21 @@ public class likou97 {
     }
 
     public boolean isInterleave(String s1, String s2, String s3) {
+        if (s1 == "" && s2 == "" && s3 == "") return true;
         if (s1.length() + s2.length() != s3.length()) return false;
         boolean[][] dp = new boolean[s1.length() + 1][s2.length() + 1];
         dp[0][0] = true;
-        for (int i = 1; i < s1.length(); i++) {
+        for (int i = 1; i <= s1.length(); i++) {
             dp[i][0] = dp[i - 1][0] && (s1.charAt(i - 1) == s3.charAt(i - 1));
         }
-        for (int i = 1; i < s1.length(); i++) {
+        for (int i = 1; i <= s2.length(); i++) {
             dp[0][i] = dp[0][i - 1] && (s2.charAt(i - 1) == s3.charAt(i - 1));
         }
         for (int i = 1; i <= s1.length(); i++) {
             for (int j = 1; j <= s2.length(); j++) {
-                dp[i][j] = (dp[i - 1][j] && (s1.charAt(i - 1) == s3.charAt(i + j - 1))) ||
-                        (dp[i][j - 1] && (s2.charAt(j - 1) == s3.charAt(i + j - 1)));
+                dp[i][j] = (dp[i - 1][j] && (s1.charAt(i - 1) == s3.charAt(i + j - 1))) || (
+                        dp[i][j - 1] && (s2.charAt(j - 1) == s3.charAt(i + j - 1))
+                );
             }
         }
         return dp[s1.length()][s2.length()];
@@ -51,7 +53,6 @@ public class likou97 {
             return false;
         }
         boolean[][] f = new boolean[n + 1][m + 1];
-
         f[0][0] = true;
         for (int i = 0; i <= n; ++i) {
             for (int j = 0; j <= m; ++j) {
