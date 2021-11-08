9d1a6cd977e7f3fd24ae3c6d0fdfbf32bc95ff9d
qq422162318 <422162318@qq.com>
2021-11-08 08:59:47 +0800

    jkl

--- src/main/java/算法题/likou97.java
+++ src/main/java/算法题/likou97.java
@@ -23,11 +23,10 @@ public class likou97 {
     public static void main(String[] args) {
         System.out.println(true & false);
         likou97 likou97 = new likou97();
-        System.out.println(likou97.isInterleave("aabcc", "dbbca", "aadbbcbcac"));
+        System.out.println(likou97.isInterleave("a", "", "-"));
     }
 
     public boolean isInterleave(String s1, String s2, String s3) {
-        if (s1 == "" && s2 == "" && s3 == "") return true;
         if (s1.length() + s2.length() != s3.length()) return false;
         boolean[][] dp = new boolean[s1.length() + 1][s2.length() + 1];
         dp[0][0] = true;
@@ -39,10 +38,32 @@ public class likou97 {
         }
         for (int i = 1; i <= s1.length(); i++) {
             for (int j = 1; j <= s2.length(); j++) {
-                dp[i][j] = dp[i - 1][j] && (s2.charAt(j - 1) == s3.charAt(i - 1));
+                dp[i][j] = (dp[i - 1][j] && (s1.charAt(i - 1) == s3.charAt(i + j - 1))) ||
+                        (dp[i][j - 1] && (s2.charAt(j - 1) == s3.charAt(i + j - 1)));
             }
         }
         return dp[s1.length()][s2.length()];
     }
 
+    public boolean isInterleave2(String s1, String s2, String s3) {
+        int n = s1.length(), m = s2.length(), t = s3.length();
+        if (n + m != t) {
+            return false;
+        }
+        boolean[][] f = new boolean[n + 1][m + 1];
+
+        f[0][0] = true;
+        for (int i = 0; i <= n; ++i) {
+            for (int j = 0; j <= m; ++j) {
+                int p = i + j - 1;
+                if (i > 0) {
+                    f[i][j] = f[i][j] || (f[i - 1][j] && s1.charAt(i - 1) == s3.charAt(p));
+                }
+                if (j > 0) {
+                    f[i][j] = f[i][j] || (f[i][j - 1] && s2.charAt(j - 1) == s3.charAt(p));
+                }
+            }
+        }
+        return f[n][m];
+    }
 }
